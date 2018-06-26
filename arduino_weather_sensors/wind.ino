78869afc667a23b0467aad832049c41db5f7b258
justinbeharry <jsbeharr@umich.edu>
2018-06-26 14:40:03 -0400

    Clean wind code
    *rename variables
    *delete unecessary variables

--- arduino_weather_sensors/wind.ino
+++ arduino_weather_sensors/wind.ino
@@ -1,15 +1,14 @@
 // Adafruit Wind Anemometer
 
 //Setup Variables
-int sensorValue = 0; //Variable stores the value direct from the analog pin
-float sensorVoltage = 0; //Variable that stores the voltage (in Volts) from the anemometer being sent to the analog pin
-float windSpeed = 0; // Wind speed in meters per second (m/s)
+int sensor_value = 0; //Variable stores the value direct from the analog pin
+float sensor_voltage = 0; //Variable that stores the voltage (in Volts) from the anemometer being sent to the analog pin
+float wind_speed = 0; // Wind speed in meters per second (m/s)
 
 //Anemometer Technical Variables
-float voltageMin = .4; // Mininum output voltage from anemometer in mV.
-float windSpeedMin = 0; // Wind speed in meters/sec corresponding to minimum voltage
-float voltageMax = 2.0; // Maximum output voltage from anemometer in mV.
-float windSpeedMax = 32; // Wind speed in meters/sec corresponding to maximum voltage
+const float VOLTAGE_MIN = .4; // Mininum output voltage from anemometer in mV.
+const float VOLTAGE_MAX = 2.0; // Maximum output voltage from anemometer in mV.
+const float WIND_SPEED_MAX = 32; // Wind speed in meters/sec corresponding to maximum voltage
 
 float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -17,22 +16,22 @@ float mapFloat(float x, float in_min, float in_max, float out_min, float out_max
 
 void wind_run()
 {
-  sensorValue = analogRead(WINDPIN); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
+  sensor_value = analogRead(WINDPIN); //Get a value between 0 and 1023 from the analog pin connected to the anemometer
 
-  sensorVoltage = mapFloat(sensorValue, 0, 1023, 0, 5);
+  sensor_voltage = mapFloat(sensor_value, 0, 1023, 0, 5);
 
   //Convert voltage value to wind speed using range of max and min voltages and wind speed for the anemometer
-  if (sensorVoltage <= voltageMin) {
-    windSpeed = 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
+  if (sensor_voltage <= VOLTAGE_MIN) {
+    wind_speed = 0; //Check if voltage is below minimum value. If so, set wind speed to zero.
   } else {
-    windSpeed = (sensorVoltage - voltageMin) * windSpeedMax / (voltageMax - voltageMin); //For voltages above minimum value, use the linear relationship to calculate wind speed.
+    wind_speed = (sensor_voltage - VOLTAGE_MIN) * WIND_SPEED_MAX / (VOLTAGE_MAX - VOLTAGE_MIN); //For voltages above minimum value, use the linear relationship to calculate wind speed.
   }
 
-  //Print voltage and windspeed to serial
+  //Print voltage and wind_speed to serial
   Serial.print("Voltage: ");
-  Serial.print(sensorVoltage);
+  Serial.print(sensor_voltage);
   Serial.print("\t");
   Serial.print("Wind speed: ");
-  Serial.println(windSpeed);
+  Serial.println(wind_speed);
 
 }
