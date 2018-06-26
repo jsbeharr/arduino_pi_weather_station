7265a6f10f079375ff1e96905dea741abd6ec5c9
justinbeharry <jsbeharr@umich.edu>
2018-06-26 14:48:06 -0400

    Clean rain code
    *made const variables capitalized
    *changed variables to camel case

--- arduino_weather_sensors/rain.ino
+++ arduino_weather_sensors/rain.ino
@@ -1,16 +1,14 @@
 // Sunfounder Raindrop sensor
 
-// lowest and highest sensor readings:
-const int sensorMin = 0;    // sensor minimum
-const int sensorMax = 1024; // sensor maximum
+// lowest and highest sensor readings
+const int SENSOR_MIN = 0;
+const int SENSOR_MAX = 1024;
 
 void rain_run()
 {
+  int sensor_reading = analogRead(RAINPIN);
+  int range = map(sensor_reading, SENSOR_MIN, SENSOR_MAX, 0, 3);
 
-  int sensorReading = analogRead(RAINPIN);
-  int range = map(sensorReading, sensorMin, sensorMax, 0, 3);
-
-  // range value:
   switch (range)
   {
   case 0: // Sensor getting wet
@@ -23,5 +21,6 @@ void rain_run()
     Serial.println("Not Raining");
     break;
   }
-  Serial.println(sensorReading);
+  
+  Serial.println(sensor_reading);
 }
