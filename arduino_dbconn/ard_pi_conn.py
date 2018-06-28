9069d65bd4967b84592bb4fa982724d80b4b57c4
Justin Beharry <jsbeharr@umich.edu>
2018-06-28 20:28:06 +0000

    Connect to databse
    use mysql connector to connect to databse

--- arduino_dbconn/ard_pi_conn.py
+++ arduino_dbconn/ard_pi_conn.py
@@ -1,12 +1,22 @@
 import serial
 import time
 import logging
+import mysql.connector
+from mysql.connector import errorcode
 
 # Sets up a logger
 logging.basicConfig(
         filename='sensor_test.log',
         level=logging.DEBUG,
         format='%(asctime)s:%(levelname)s:%(message)s')
+
+# database config
+config = {
+        'user': 'root',
+        'password': 'test_pass',
+        'host': 'localhost',
+        'database': 'arduino_pi_weather_station_dev'
+        }
 
 # Specifies which serial port to listen on
 arduino = serial.Serial('/dev/ttyACM0', 9600)
@@ -32,3 +42,24 @@ logging.debug('Wetness: {}'     .format(wetness))
 logging.debug('Wind Speed: {} ' .format(wind_speed))
 logging.debug('Pressure: {}'    .format(pressure)) 
 logging.debug('Temperature: {}' .format(temperture))
+
+cnx = cur = None
+try:
+    cnx = mysql.connector.connect(**config)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print('Something is wrong with your user name or password')
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    cur = cnx.cursor()
+    cur.execute('show databases;')
+    for row in cur.fetchall():
+        print(row)
+finally:
+    if cur:
+        cur.close()
+    if cnx:
+        cnx.close()
